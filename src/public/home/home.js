d1d5dd0e1504c48afc2f593cf74b5cb4418d96f7
cytescale <nikhil@cytescale.com>
2020-11-10 11:45:50 +0530

    top bar commit

--- src/public/home/home.js
+++ src/public/home/home.js
@@ -200,7 +200,10 @@ return(
             <div id='home_app_login_butt_bdy'>
             {
                 this.state.windowWidth<650?
-                !this.state.isLogged?<input type='submit' value='Login or Sign Up' id='lb_first_stack_right_cont_cent_login_data_login_butt_home'></input>
+                !this.state.isLogged?
+                
+                <input type='submit' value='Login or Sign Up' id='lb_first_stack_right_cont_cent_login_data_login_butt_home'></input>
+                
                 :<div id='app_bottom_bar_main_bdy'>
                      <div className="app_bottom_bar_main_bdy_col">
                          <a href='/feed' style={this.state.currSite==1?{color:'var(--app-primary-color)'}:{}} className='home_app_bdy_cont_left_cont_opt_butt_class'><svg className='home_app_bdy_cont_left_cont_opt_butt_img_class' viewBox='0 0 512 512'><path d='M368 415.86V72a24.07 24.07 0 00-24-24H72a24.07 24.07 0 00-24 24v352a40.12 40.12 0 0040 40h328' fill='none' stroke='currentColor' stroke-linejoin='round' strokeWidth='32'/><path d='M416 464h0a48 48 0 01-48-48V128h72a24 24 0 0124 24v264a48 48 0 01-48 48z' fill='none' stroke='currentColor' stroke-linejoin='round' strokeWidth='32'/><path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' strokeWidth='32' d='M240 128h64M240 192h64M112 256h192M112 320h192M112 384h192'/><path d='M176 208h-64a16 16 0 01-16-16v-64a16 16 0 0116-16h64a16 16 0 0116 16v64a16 16 0 01-16 16z' fill='currentColor'/></svg>{this.state.windowWidth>650?<span className='home_app_bdy_cont_left_cont_opt_butt_tit_class'>Feed</span>:<span></span>}</a>
