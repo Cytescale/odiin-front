e1d1f476c9d3c3e2e81bb5dbe01b4ca4a3d99c30
cytescale <nikhil@cytescale.com>
2020-11-11 19:46:04 +0530

    order commit

--- src/public/home/home.js
+++ src/public/home/home.js
@@ -94,7 +94,7 @@ export default class HomeClass extends React.Component{
                                </div>
                                <div className='home_app_bdy_order_cont'>
                                <div className='home_app_bdy_order_opt_bdy'> 
-                                                          <a href='#' className='home_app_bdy_cost_cal_opt_bdy_lnk'><svg className='home_app_left_bdy_ico'  viewBox='0 0 512 512'><title>Wallet</title><rect x='48' y='144' width='416' height='288' rx='48' ry='48' fill='none' stroke='currentColor' stroke-linejoin='round' stroke-width='22'/><path d='M411.36 144v-30A50 50 0 00352 64.9L88.64 109.85A50 50 0 0048 159v49' fill='none' stroke='currentColor' stroke-linejoin='round' stroke-width='22'/><path fill='currentColor' stroke='currentColor'  d='M368 320a32 32 0 1132-32 32 32 0 01-32 32z'/></svg>Cost Callculator</a>
+                                                          <a href='#' className='home_app_bdy_cost_cal_opt_bdy_lnk'>Logout</a>
                                                     </div>
                                    
                                </div>
--- src/public/order/order.js
+++ src/public/order/order.js
@@ -27,8 +27,7 @@ export default class OderModel extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            changePassModalVisi:false,
-            deacAccModalVisi:false
+            layNum:0
           }        
           props.onChangeSite(3);
 
@@ -44,7 +43,9 @@ export default class OderModel extends React.Component{
                                 <div id='app_setting_main_bdy_tit_main_sub_tit'>
                                     Make a new order
                                 </div>
-
+                                <Alert  className='app_order_new_alrt_cont' variant="danger">
+                                        Currently you can order only android application layouts
+                                   </Alert>
                                 <div className='app_billing_main_cont'>
                                     <div className='app_billing_main_cont_tit'>
                                         Order Details
@@ -60,20 +61,90 @@ export default class OderModel extends React.Component{
 
 
                                         <div className='app_setting_main_bdy_tit_main_sub_opt_cont'>
-                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Project Category</div>
-                                             <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Category</div>
+                                             <DropdownButton id="dropdown-basic-button" variant="light" title="Select Category">
                                         <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
                                         <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
                                         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
                                         </DropdownButton>
                                         </div>
 
+                                        <div className='app_setting_main_bdy_tit_main_sub_opt_cont'>
+                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Structure</div>
+                                             <DropdownButton id="dropdown-basic-button" title="Select Structure">
+                                        <Dropdown.Item href="#/action-1">Java/xml environment</Dropdown.Item>
+                                        <Dropdown.Item href="#/action-2">React native environment</Dropdown.Item>
                                         
+                                        </DropdownButton>
+                                        </div>
+                                        <div className='app_setting_main_bdy_tit_main_sub_opt_cont'>
+                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Number of layouts (Activities)</div>
+                                             <div className='app_order_num_lay_data_cont'>
+                                                  <div className='app_order_num_lay_data'>0</div>
+                                                  <div className='app_order_num_lay_data_butt'><Button id='app_order_num_lay_data_butt_min'>-</Button><Button id='app_order_num_lay_data_butt_pl'>+</Button></div>
+                                             </div>
+                                        </div>
+                                        <div className='app_setting_main_bdy_tit_main_sub_opt_cont'>
+                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Notes for developers</div>
+                                             <div className='app_order_num_lay_data_cont'>
+                                                  <textarea className='app_order_dec_note_text_ar'> </textarea>
+                                             </div>
+                                        </div>
+                                        <div className='app_setting_main_bdy_tit_main_sub_opt_cont'>
+                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Design Files</div>
+                                             <div className='app_order_num_lay_data_cont'>
+                                             <div className='app_order_des_lay_data'>Add any type of design files</div>
+                                                  <div className='app_order_num_lay_data_butt'><Button type='' id='app_order_num_des_data_butt_min'>add files</Button></div>
+                                             </div>
+                                        </div>
 
+                                        <div className='app_setting_main_bdy_tit_main_sub_opt_cont'>
+                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Github repository link</div>
+                                             <input type='text' placeholder='Enter github repository link' className='app_setting_main_bdy_tit_main_sub_opt_cont_data'></input>
+                                        </div>
+                                        <div className='app_setting_main_bdy_tit_main_sub_opt_cont'>
+                                             <div className='app_setting_main_bdy_tit_main_sub_opt_cont_tit'>Alternate contact email address</div>
+                                             <input type='text' placeholder='Enter alernate email address' className='app_setting_main_bdy_tit_main_sub_opt_cont_data'></input>
+                                        </div>
 
                                 </div>
-                             
+                                <div className='app_billing_main_cont'>
+                                    <div className='app_billing_main_cont_tit'>
+                                        Checkout details
+                                    </div>
+                                    <div className='app_billing_main_cont_sub_tit'>
+                                        All the checkout details based on your project description
+                                    </div>
+                                    <Alert  className='app_order_new_alrt_cont' variant="info">
+                                        After checkout , only 50% amount of upfront payment will be repaid in case of cancelation
+                                   </Alert>
+
+                                   <div className='app_order_chk_tit_main_cont'>
+                                        <div className='app_order_chk_tit_main_det_cont'>Details</div>
+                                        <div className='app_order_chk_tit_main_char_cont'>Charges</div>
+                                   </div>
+                                   <div id='app_order_det_data'>data</div>
+                                   <div className='app_order_sub_cont'>
+                                   <div className='app_order_sub_main_cont'>
+                                        <div className='app_order_sub_main_cont_tit_cont'>Sub Total</div>
+                                        <div className='app_order_sub_main_cont_cost_cont'>$0.00</div>
+                                   </div>
+                                   <div className='app_order_sub_main_cont'>
+                                        <div className='app_order_sub_main_cont_tit_cont'>Estimate Total</div>
+                                        <div className='app_order_sub_main_cont_cost_cont'>$0.00</div>
+                                   </div>
+                                   </div>
+
+                                   <div className='app_order_up_main_cont'>
+                                        <div className='app_order_up_main_cont_tit'>Current upfront amount</div>
+                                        <div className='app_order_up_main_cont_cost'>$0.00</div>
+                                   </div>
+
+                                </div>
+
+                                <Button variant='success' id='a_o_c_b'>Checkout</Button>
                     </div>
+                    
           </div>  
         );
     }
