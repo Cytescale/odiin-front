13698c53bda34494bce97870e9d879764033d4d2
cytescale <nikhil@cytescale.com>
2021-01-05 22:44:56 +0530

    dev

--- src/public/land/land.js
+++ src/public/land/land.js
@@ -637,7 +637,7 @@ export default class LandClass extends React.Component{
                               </div>
                               <div className='app_land_stack_15_lft_1_get_butt'>
                               
-                              <a className='app_land_stack_15_lft_1_get_butt_lnk' href='https://www.cytescale:3000/contact'>Contact Us</a>
+                              <a className='app_land_stack_15_lft_1_get_butt_lnk' href='https://www.cytescale.com/contact'>Contact Us</a>
                          </div>
                     </div>
                     
